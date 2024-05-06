42a323e38de216cc633e50513ff45640abe2fc66
skalex stuch <skalexstuch@me.com>
2024-05-06 12:33:13 +0800

    desktop size finished

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the input element
+    var input = document.getElementById("name");
+
+    // Clear its value
+    input.value = "";
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the input element
+    var input = document.getElementById("email");
+
+    // Clear its value
+    input.value = "";
+});
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the input element
+    var input = document.getElementById("message");
+
+    // Clear its value
+    input.value = "";
+});
