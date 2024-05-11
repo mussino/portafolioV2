6e7da28c0124df9425b61409e735dc71656543d2
skalex stuch <skalexstuch@me.com>
2024-05-11 11:05:13 +0800

    update side bar and portfolio scrolls

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+const toggleBtn = document.getElementById('toggleBtn')
+const sideBar = document.querySelector('.sidebar')
+const xclose = document.getElementById('xclose')
+const sidebarWelcome = document.querySelector('#sidebar-welcome')
+const sidebarSkills = document.querySelector('#sidebar-skills')
+const sidebarPortfolio = document.querySelector('#sidebar-portfolio')
+const sidebarAbout = document.querySelector('#sidebar-about')
+const sidebarContact = document.querySelector('#sidebar-contact')
+
+
+
+toggleBtn.onclick = showSidebar
+xclose.onclick = hideSidebar
+sidebarWelcome.onclick = hideSidebar
+sidebarSkills.onclick = hideSidebar
+sidebarPortfolio.onclick = hideSidebar
+sidebarAbout.onclick = hideSidebar
+sidebarContact.onclick = hideSidebar
+
+
+
+
+// Funtions to show / hide the side bar
+function showSidebar() {
+    sideBar.style.display = 'flex'
+}
+function hideSidebar() {
+    sideBar.style.display = 'none'
+}
+
+
+// Functions to clear the inputs from the Form
 document.addEventListener("DOMContentLoaded", function() {
     // Function to clear input value
     function clearInputValue(inputId) {
@@ -12,20 +44,3 @@ document.addEventListener("DOMContentLoaded", function() {
     clearInputValue("email");
     clearInputValue("message");
 });
-
-const toggleBtn = document.getElementById('toggleBtn')
-const sideBar = document.querySelector('.sidebar')
-const xclose = document.getElementById('xclose')
-
-toggleBtn.onclick = showSidebar
-
-function showSidebar() {
-    sideBar.style.display = 'flex'
-}
-
-
-function hideSidebar() {
-    sideBar.style.display = 'none'
-}
-
-xclose.onclick = hideSidebar
